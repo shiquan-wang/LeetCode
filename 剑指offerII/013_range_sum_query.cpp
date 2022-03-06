f9c0602dffa952733f04b42f6c31450db4898b9f
summertree <1490340776@qq.com>
2022-03-06 11:35:35 +0800

    013

--- 剑指offerII/013_range_sum_query.cpp
+++ 剑指offerII/013_range_sum_query.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-class NumArray{
+class NumArray1D{
 public:
     vector<int> sums;
-    NumArray(vector<int>& nums){
+    NumArray1D(vector<int>& nums){
         int n = nums.size();
         sums.resize(n+1);
         sums[0] = 0;
@@ -21,4 +21,30 @@ public:
     }
 
 
+};
+
+class NumMatrix{
+public:
+    vector<vector<int>> sums;
+
+    NumMatrix(vector<vector<int>> matrix){
+        int m = matrix.size();
+        if(m > 0){
+            int n = matrix[0].size();
+            sums.resize(m, vector<int>(n+1));
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    sums[i][j+1] = sums[i][j] + matrix[i][j];
+                }
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2){
+        int sum = 0;
+        for(int i = row1; i <= row2; ++i){
+            sum += (sums[i][col2 + 1] - sums[i][col1]);
+        }
+        return sum;
+    }
 };
