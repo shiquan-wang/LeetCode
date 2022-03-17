45550a29b1b5b6a75ace589870b3946514006960
summertree <1490340776@qq.com>
2022-03-17 21:54:34 +0800

    ListNode.h

---
+++ List/c++/ListNode.h
@@ -0,0 +1,8 @@
+struct ListNode
+{
+    int val;
+    ListNode *next = new ListNode();
+    ListNode():val(0), next(nullptr){}
+    ListNode(int val):val(val), next(nullptr){}
+    ListNode(int val, ListNode *next):val(val), next(next){}
+};
